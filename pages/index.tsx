a61496fa9c31eac251708ab8d71e4e4bc09fad43
yuval-david <yuvid0224@gmail.com>
2023-08-12 11:36:06 +0300

    create connection with mysqlDB

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  async function getData(){
+    const apiEndPoint = "http://localhost:3000/api/getData";
+    const response = await fetch(apiEndPoint);
+    const resJson = await response.json();
+    console.log(resJson);
+  };
+
+  useEffect(()=>{
+    getData();
+  },[]);
+
   return (
     <>
       <Head>
