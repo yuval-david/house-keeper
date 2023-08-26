5f8714dcac6afe19d9c358ee3bc7313f26d30cdb
yuval-david <yuvid0224@gmail.com>
2023-08-27 00:06:46 +0300

    Make homepage responsive

--- components/home/HomePageComponent.tsx
+++ components/home/HomePageComponent.tsx
@@ -9,30 +9,32 @@ export function HomePageComponent() {
                 <Link href="/" className={style.center}>
                     <h2>במה תרצה להתחיל?</h2>
                 </Link>
-                <Link href="/" className={style.deg0}>
-                    <span className={style.yellow}>דוחות</span>
-                </Link>
-                <Link href="/" className={style.deg45}>
-                    <span className={style.red}>תקלות</span>
-                </Link>
-                <Link href="/" className={style.deg90}>
-                    <span className={style.disabled}>פרטי בניין</span>
-                </Link>
-                <Link href="/" className={style.deg135}>
-                    <span className={style.green_zit}>חברת ניהול</span>
-                </Link>
-                <Link href="/" className={style.deg180}>
-                    <span className={style.disabled}>תשלומים</span>
-                </Link>
-                <Link href="/" className={style.deg225}>
-                    <span className={style.blue}>ניהול דיירים</span>
-                </Link>
-                <Link href="/" className={style.deg270}>
-                    <span className={style.disabled}>סקרים</span>
-                </Link>
-                <Link href="/" className={style.deg315}>
-                    <span className={style.green}>פגישות דיירים</span>
-                </Link>
+                <div className={style.links_container}>
+                    <Link href="/" className={style.deg0}>
+                        <span className={style.yellow}>דוחות</span>
+                    </Link>
+                    <Link href="/" className={style.deg45}>
+                        <span className={style.red}>תקלות</span>
+                    </Link>
+                    <Link href="/" className={style.deg90}>
+                        <span className={style.disabled}>פרטי בניין</span>
+                    </Link>
+                    <Link href="/" className={style.deg135}>
+                        <span className={style.green_zit}>חברת ניהול</span>
+                    </Link>
+                    <Link href="/" className={style.deg180}>
+                        <span className={style.disabled}>תשלומים</span>
+                    </Link>
+                    <Link href="/" className={style.deg225}>
+                        <span className={style.blue}>ניהול דיירים</span>
+                    </Link>
+                    <Link href="/" className={style.deg270}>
+                        <span className={style.disabled}>סקרים</span>
+                    </Link>
+                    <Link href="/" className={style.deg315}>
+                        <span className={style.green}>פגישות דיירים</span>
+                    </Link>
+                </div>
             </div>
         </div>
     )
