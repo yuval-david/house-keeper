afc2c4f74311f61cc87fa89c49752fb798aaf316
yuval-david <yuvid0224@gmail.com>
2023-09-08 23:44:35 +0300

    Insert meetings

--- pages/api/v1/buildings/[building_id]/meetings/index.ts
+++ pages/api/v1/buildings/[building_id]/meetings/index.ts
@@ -5,9 +5,27 @@ export default async function handler(
     request: NextApiRequest,
     response: NextApiResponse,
 ) {
+
     try {
-        const meetings = await sql`SELECT * FROM meetings;`;
-        return response.status(200).json({ meetings: meetings.rows });
+        // GET 
+        if (request.method === 'GET') {
+            const meetings = await sql`SELECT * FROM meetings;`;
+            return response.status(200).json({ meetings: meetings.rows });
+        }
+
+        // POST
+        // if (request.method === 'POST') {
+        //     const meetingName = request.query.meetingName as string;
+        //     const meetingDate = request.query.meetingDate as string;
+        //     const location = request.query.location as string;
+        //     const description = request.query.meetingDate as string;
+        //     if (!meetingName || !meetingDate || !location) throw new Error('Meeting details are missing.');
+        //     await sql`INSERT INTO meetings (name, date, location, description) VALUES (${meetingName}, ${meetingDate}, ${location}, ${description});`;
+
+        //     const meetings = await sql`SELECT * FROM meetings;`;
+        //     return response.status(200).json({ meetings: meetings.rows });
+        // }
+
     } catch (error) {
         return response.status(500).json({ error });
     }
--- postgres_commends_history.sql
+++ postgres_commends_history.sql
@@ -9,3 +9,14 @@ CREATE TABLE meetings (
 
 ALTER TABLE meetings
 ADD COLUMN users int[];
+
+INSERT INTO meetings (
+   name, 
+   date, 
+   location, 
+   description
+)
+VALUES
+('הסדרי חנייה', '2023-10-18', 'לובי', 'בפגישה נדון בנושא הסדרי החנייה, ונעלה פתרונות.'),
+('שיפוץ 2024', '2023-11-25', 'דירה 14', ''),
+('פגישה חודשית', '2023-10-06', 'לובי', 'בפגישה דנו בניקיון הבניין.');
