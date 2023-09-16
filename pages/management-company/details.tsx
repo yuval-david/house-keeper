bc112887bef56fcca3d8e823ea4c7941fa9589a0
yuval-david <yuvid0224@gmail.com>
2023-09-16 15:52:36 +0300

    order pages & build management comp page

---
+++ pages/management-company/details.tsx
@@ -0,0 +1,10 @@
+import { PageLayout } from '@/components/UI/PageLayout'
+import React from 'react'
+
+export default function DetailsPage() {
+    return (
+        <PageLayout>
+            Management Company - Details
+        </PageLayout>
+    )
+}
